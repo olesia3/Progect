3eec8146440f3f1c8c80b1a8f02924b269c83c9d
olesia3 <olesiawww34@gmail.com>
2022-05-27 11:21:38 +0300

    added files

---
+++ src/Package/Task1.java
@@ -0,0 +1,7 @@
+package Package;
+public class Task1 {
+	public static void main(String[] args) {
+		int i=(int)(Math.random()* 30 -15);
+		System.out.println("Випадкове натуральне число"+i);
+	}
+}
---
+++ src/Package/Task2.java
@@ -0,0 +1,13 @@
+package Package;
+public class Task2 {
+int t1;
+double t2;
+public Task2(int t1, double t2) {
+	this.t1 = t1;
+	this.t2 = t2;
+}
+public Task2(double t2) {
+	this.t2 = t2;
+}
+
+}
